572617b69de0e906d267b0e1ba10b0a88a29ba8d
varshini <dreamvarshini@gmail.com>
2021-09-01 01:56:56 +0530

    ADDED:sample queue and added elements

---
+++ src/com/bridgelabz/stackandqueues/QueueMain.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.stackandqueues;
+
+import com.bridgelabz.linkedlist.MyNodeImpl;
+
+public class QueueMain {
+
+	public static void main(String[] args) 
+	
+	{
+		MyQueue<Integer> queue = new MyQueue<Integer>();
+		MyNodeImpl<Integer> firstNode = new MyNodeImpl<Integer>(70);
+		MyNodeImpl<Integer> secondNode= new MyNodeImpl<Integer>(30);
+		MyNodeImpl<Integer> thirdNode = new MyNodeImpl<Integer>(56);
+		
+		queue.enqueue(firstNode);
+		queue.enqueue(secondNode);
+		queue.enqueue(thirdNode);
+		queue.printQueue();
+
+	}
+
+}
